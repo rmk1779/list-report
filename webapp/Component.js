ad14d9f5b2bb8f92f1905eb50ee2dced6be4b7a6
rmk1779 <rrmk17@gmail.com>
2020-06-30 06:12:28 +0000

    1st commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("LR.list_report.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
